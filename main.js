3cde8f9240a05fc9882941ce982c69d55c264261
unknown <deltondmello555@gmail.com>
2021-03-15 12:38:43 +0000

    links to pages fixed, images added, and font chaged to chiller, border made rectangular shaped

--- main.js
+++ main.js
@@ -36,3 +36,5 @@ darkModeToggle.addEventListener('click', () => {
     disableDarkMode(); 
   }
 });
+
+// the above code was obtained from - https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/
