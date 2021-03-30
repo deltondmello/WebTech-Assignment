798bd07b0f22b7a1379e43f9a43cb6c5fb1cabc3
unknown <deltondmello555@gmail.com>
2021-03-30 12:54:40 +0100

    fixed dark button, and it works consistently on every page

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-
-let darkMode = localStorage.getItem('darkMode'); 
+let darkmode = localStorage.getItem('darkMode'); 
 
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
 
@@ -7,7 +6,15 @@ const enableDarkMode = () => {
   // 1. Add the class to the body
   document.body.classList.add('darkmode');
   // 2. Update darkMode in localStorage
-  localStorage.setItem('darkMode', 'enabled');
+  localStorage.setItem('darkMode', 'enabledDark');
+
+  //update button to stay white
+  var element =  document.getElementById('dark-mode-toggle');
+  element.checked = true;  
+
+  // update toggle Button text
+  var btnText =  document.getElementById('darkBtnStyle');
+  btnText.innerHTML = 'Light-Mode'
 }
 
 const disableDarkMode = () => {
@@ -15,21 +22,35 @@ const disableDarkMode = () => {
   document.body.classList.remove('darkmode');
   // 2. Update darkMode in localStorage 
   localStorage.setItem('darkMode', null);
+
+  //update button to stay black
+  var element =  document.getElementById('dark-mode-toggle');
+  element.checked = false; 
+
+  // update toggle Button text
+  var btnText =  document.getElementById('darkBtnStyle');
+  btnText.innerHTML = 'Dark-Mode'
+
+
 }
  
+
 // If the user already visited and enabled darkMode
 // start things off with it on
-if (darkMode === 'enabled') {
+
+if (darkmode === 'enabledDark') {
   enableDarkMode();
 }
+
+
 
 // When someone clicks the button
 darkModeToggle.addEventListener('click', () => {
   // get their darkMode setting
-  darkMode = localStorage.getItem('darkMode'); 
-  
+  darkmode = localStorage.getItem('darkMode'); 
+
   // if it not current enabled, enable it
-  if (darkMode !== 'enabled') {
+  if (darkmode !== 'enabledDark') {
     enableDarkMode();
   // if it has been enabled, turn it off  
   } else {  
@@ -38,3 +59,4 @@ darkModeToggle.addEventListener('click', () => {
 });
 
 // the above code was obtained from - https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/
+// and has been edited for text appending and checking for dark button slider
